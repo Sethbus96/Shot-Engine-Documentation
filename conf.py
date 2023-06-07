6511cc2bf2ad95737a367b3f7a1b19a7aac8edb5
Russel Studios <newseth96@gmail.com>
2023-06-07 06:06:34 +0000

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,7 +1,1 @@
 extensions = ['sphinx.ext.autosectionlabel']
-html_theme_options = {
-    'navigation_depth': 4,
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'display_version': True,
-}
